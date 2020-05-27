28211be507cb92c32853179c8ed6758f4c5b6768
Juan19Jesus08 <juanjesuspadrondiaz@gmail.com>
2020-05-26 21:50:46 -0500

    Finalizacion acerca de

--- resources/views/principal/acerca.blade.php
+++ resources/views/principal/acerca.blade.php
@@ -2,5 +2,45 @@
 
 @section('contenido')
 
+<div class="breadcrumb-wrapper">
+            	<div class="container">
+                	<h1>¿Quienes somos?</h1>
+                                    </div>
+            </div>
+
+
+<div id="main">
+                <!-- main-content starts here -->
+                <div id="main-content">
+                    <section id="primary" class="content-full-width">
+                        <div class="dt-sc-hr-invisible-small"></div>
+                        <div class="dt-sc-hr-invisible-normal"></div>
+                        <div class="container">
+                            
+                            
+                           
+                            <div class="column dt-sc-one-third  space first">
+                                <h3>Misión</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sagittis elementum est, eu consectetur massa aliquam et. Sed sit amet est et turpis viverra consequat.</p>
+                            </div>
+                            <div class="column dt-sc-one-third  space ">
+                                <h3>Visión</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sagittis elementum est, eu consectetur massa aliquam et. Sed sit amet est et turpis viverra consequat.</p>
+                            </div>
+                            <div class="column dt-sc-one-third  space ">
+                                <h3>Valores</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sagittis elementum est, eu consectetur massa aliquam et. Sed sit amet est et turpis viverra consequat.</p>
+                            </div>
+
+
+
+
+
+                        </div>
+                       
+                    </section>
+				</div>
+                <!-- main-content ends here -->
+            </div>
 
 @stop
